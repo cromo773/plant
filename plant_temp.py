b5ec75b994cf919f42de99633854d832b869bbf6
cromo773 <79518463+cromo773@users.noreply.github.com>
2021-04-20 13:15:43 +0200

    Update plant_temp.py

--- plant_temp.py
+++ plant_temp.py
@@ -71,12 +71,12 @@ print("Reading MCP3008 value on channel 0, press Ctrl-C to quit...")
 for i in range(0,10):
     try:
         GPIO.output(SWITCH, GPIO.HIGH)
-        sleep(10)
+        sleep(1)
         value = float(mcp.read_adc(0))
         value2 = format(value / 1023 * 100)
         print("The soil moisture reading is currently at:",value2)
         GPIO.output(SWITCH, GPIO.LOW)
-        sleep(10)
+        sleep(1)
     except KeyboardInterrupt:
         GPIO.output(SWITCH, GPIO.LOW)
         GPIO.cleanup()
